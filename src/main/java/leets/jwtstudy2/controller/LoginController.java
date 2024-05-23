f4eb11fdcfc4c88eaa13af85fdc566092f8b8932
hyxklee <ewgt1234@naver.com>
2024-05-23 14:10:16 +0900

    파라미터로 받으면 URL에 정보가 다 공개되기 때문에 바디에 담아서 요청을 보냄

--- src/main/java/leets/jwtstudy2/controller/LoginController.java
+++ src/main/java/leets/jwtstudy2/controller/LoginController.java
@@ -1,16 +1,22 @@
 package leets.jwtstudy2.controller;
 
+import leets.jwtstudy2.domain.UserLoinDTO;
 import leets.jwtstudy2.service.UserLoginService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class LoginController {
 
     private final UserLoginService userLoginService;
@@ -21,10 +27,25 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestParam("userid") String userid, @RequestParam("password") String password) {
-        String token = userLoginService.login(userid, password);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token);
-        return ResponseEntity.ok().headers(headers).build();
+    public ResponseEntity<String> loginUser(@RequestBody UserLoinDTO dto) {
+        try {
+            String token = userLoginService.login(dto.getUserId(), dto.getPassword());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + token);
+            return ResponseEntity.ok().headers(headers).build();
+        } catch (AuthenticationException e) {
+            String errorMessage;
+            if (e instanceof UsernameNotFoundException) {
+                errorMessage = "유저없음";
+                log.error(errorMessage);
+            } else if (e instanceof BadCredentialsException) {
+                errorMessage = "비밀번호틀림";
+                log.error(errorMessage);
+            } else {
+                errorMessage = "로그인 정보를 다시 입력해주세요.";
+                log.error(errorMessage);
+            }
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
---
+++ src/main/java/leets/jwtstudy2/domain/UserLoinDTO.java
@@ -0,0 +1,11 @@
+package leets.jwtstudy2.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoinDTO {
+    private String userId;
+    private String password;
+}
