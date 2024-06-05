8f99b55f71ed8d116b819e9c3c8a6b06ff69901b
hyxklee <ewgt1234@naver.com>
2024-06-05 17:50:01 +0900

    feat: AuthenticationFailureHandlerImpl 구현

---
+++ src/main/java/leets/jwtstudy2/config/AuthenticationFailureHandlerImpl.java
@@ -0,0 +1,19 @@
+package leets.jwtstudy2.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class AuthenticationFailureHandlerImpl implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("Authentication failed : {}", exception.getMessage());
+        response.setStatus(401);
+    }
+}
