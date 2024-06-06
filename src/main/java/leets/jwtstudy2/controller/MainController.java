6bd789b58aff2c12d52a84f403237e3ee0a99304
hyxklee <ewgt1234@naver.com>
2024-06-06 22:19:47 +0900

    feat: s3 삭제 배포 테스트

--- src/main/java/leets/jwtstudy2/controller/MainController.java
+++ src/main/java/leets/jwtstudy2/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     @GetMapping("/")
     public String index() {
-        return "main controller";
+        return "jonnassae";
     }
 
     @GetMapping("/admin")
