cc294a4f60cac57bebcc5ea4b262c31c08be98ef
hyxklee <ewgt1234@naver.com>
2024-06-07 12:08:24 +0900

    feat: log 파일 테스트 re re

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ cd $REPOSITORY
 APP_NAME=cicdtest
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+LOG_PATH=$REPOSITORY/build/libs/app.log
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
@@ -19,4 +20,4 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > Log.log 2>&1 < /dev/null &
+nohup java -jar $JAR_PATH > $LOG_PATH 2>&1 < /dev/null &
